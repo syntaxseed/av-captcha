8ad252684723e08942080e8467ecee326741d7da
Syntaxseed <3867949-syntaxseed@users.noreply.gitlab.com>
2020-03-06 11:39:39 -0500

    Tidy up code

--- avcaptcha.php
+++ avcaptcha.php
@@ -12,14 +12,15 @@ $NoiseAmount = 200; // Amount of pixel 'noise' to add.
 $FontPath = "resources/fonts/Viga-Regular.ttf";
 // **************************
 
-$NewImage =imagecreatefromjpeg("resources/img.jpg");
+$NewImage = imagecreatefromjpeg("resources/img.jpg");
 
-$LineColor = imagecolorallocate($NewImage, 80, 80, 80);  // line color
-$TextColor = imagecolorallocate($NewImage, 0, 0, 0);  // text color black
-$BackgroundColor = imagecolorallocate($NewImage, 230, 230, 240); // Image background color.
+$LineColor = imagecolorallocate($NewImage, 80, 80, 80);             // line color
+$TextColor = imagecolorallocate($NewImage, 0, 0, 0);                // Text color black
+$BackgroundColor = imagecolorallocate($NewImage, 230, 230, 240);    // Image background color
 
+// Create a random string and save to session:
 $RandomStr = substr(str_shuffle(str_repeat("0123456789abcdefghijklmnopqrstuvwxyz", $NumChars)), 0, $NumChars);
-$_SESSION['captchakey'] = $RandomStr;  // carry the data through session
+$_SESSION['captchakey'] = $RandomStr;
 
 imagefill($NewImage, 0, 0, $BackgroundColor);
 imagesetthickness($NewImage, $LineThickness);
@@ -28,26 +29,25 @@ $imgWidth = imagesx($NewImage);
 $imgHeight = imagesy($NewImage);
 
 // Draw the random lines:
-for($i = 1; $i <= $NumberOfLines; $i++){
-	$startX = rand(0, $imgWidth);
-	$startY = 0;
-	$endX = rand(0, $imgWidth);
-	$endY = $imgHeight;
-	imageline($NewImage, $startX, $startY, $endX, $endY, $LineColor);
+for ($i = 1; $i <= $NumberOfLines; $i++) {
+    $startX = rand(0, $imgWidth);
+    $startY = 0;
+    $endX = rand(0, $imgWidth);
+    $endY = $imgHeight;
+    imageline($NewImage, $startX, $startY, $endX, $endY, $LineColor);
 }
 
+// Write the text onto the image:
 imagettftext($NewImage, $FontSize, 0, 10, 18, $TextColor, $FontPath, $RandomStr);
 
 // Draw the noise over the text:
-for($i = 1; $i<=$NoiseAmount; $i++){
-	$SpeckColor = imagecolorallocate($NewImage, rand(1,255), rand(1,255), rand(1,255));
-	$SpeckX = rand(1, $imgWidth);
-	$SpeckY = rand(1, $imgHeight);
-	imagesetpixel($NewImage, $SpeckX, $SpeckY, $SpeckColor);
+for ($i = 1; $i<=$NoiseAmount; $i++) {
+    $SpeckColor = imagecolorallocate($NewImage, rand(1, 255), rand(1, 255), rand(1, 255));
+    $SpeckX = rand(1, $imgWidth);
+    $SpeckY = rand(1, $imgHeight);
+    imagesetpixel($NewImage, $SpeckX, $SpeckY, $SpeckColor);
 }
 
-//header("Content-type: image/png");
-//imagepng($NewImage);  //Output image to browser
-
+// Send headers and output to browser:
 header("Content-type: image/jpeg");
-imagejpeg($NewImage);  //Output image to browser
+imagejpeg($NewImage);
--- example.php
+++ example.php
@@ -15,28 +15,24 @@ define("AVCAPTCHA_WEB_ROOT", "/"); // Web path root to avcaptcha files.
 
 <?php
 // Check if Captcha is correct.
-if( isset($_POST['submit']) ){
+if (isset($_POST['submit'])) {
+    $key = $_SESSION['captchakey'];
+    $guess = $_POST['avcaptchaguess'];
 
-      $key = $_SESSION['captchakey'];
-      $guess = $_POST['avcaptchaguess'];
-
-      if($guess!=$key){
-          echo('Captcha test failed.');
-      }else{
-           echo('Captcha passed.');
-      }
+    if ($guess!=$key) {
+        echo('Captcha test failed.');
+    } else {
+        echo('Captcha passed.');
+    }
 }
 ?>
 
 <form method="post" action="<?php echo($_SERVER['PHP_SELF']);?>">
-<br />
-<img src="<?php echo(AVCAPTCHA_WEB_ROOT);?>avcaptcha.php" id="avcaptchaimage" style="vertical-align:middle;border:1px solid #000000;" alt="Anti-Spam Test" />&nbsp;
-<a onclick="document.getElementById('avcaptchaimage').src='<?php echo(AVCAPTCHA_WEB_ROOT);?>avcaptcha.php?ran=' + Math.random();" href="javascript:void(0);"><img src="<?php echo(AVCAPTCHA_WEB_ROOT);?>resources/reload.png" style="vertical-align:middle;" alt="Reload Captcha" title="Reload Captcha" border="0" /></a><br />
-<input name="avcaptchaguess" id="avcaptchaguess" type="text" size="15" /><br />
-<input name="submit" type="submit" value="Submit" /><br />
-
+    <img src="<?php echo(AVCAPTCHA_WEB_ROOT);?>avcaptcha.php" id="avcaptchaimage" style="vertical-align:middle;border:1px solid #000000;" alt="Anti-Spam Test" />&nbsp;
+    <a onclick="document.getElementById('avcaptchaimage').src='<?php echo(AVCAPTCHA_WEB_ROOT);?>avcaptcha.php?ran=' + Math.random();" href="javascript:void(0);"><img src="<?php echo(AVCAPTCHA_WEB_ROOT);?>resources/reload.png" style="vertical-align:middle;" alt="Reload Captcha" title="Reload Captcha" border="0" /></a><br />
+    <input name="avcaptchaguess" id="avcaptchaguess" type="text" size="15" /><br />
+    <input name="submit" type="submit" value="Submit" />
 </form>
-
 
 </body>
 </html>
